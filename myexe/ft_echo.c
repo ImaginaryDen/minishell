d2f7b72314de07899dd7eaa7b24643da350e44a6
ImaginaryDen <denis.marakov@mail.ru>
2022-02-17 19:35:05 +0300

    split ft_echo

--- myexe/ft_echo.c
+++ myexe/ft_echo.c
@@ -1,5 +1,25 @@
 #include "minishell.h"
 
+int	skip_flag(char **arg, int *i)
+{
+	int	j;
+	int is_n;
+
+	is_n = 0;
+	while(arg[*i])
+	{
+		j = 1;
+		if (arg[*i][0] == '-')
+			while (arg[*i][j] && arg[*i][j] == 'n')
+				j++;
+		if (arg[*i][j] != '\0' || j == 1)
+			break;
+		is_n = 1;
+		*i += 1;
+	}
+	return (is_n);
+}
+
 void	ft_echo(char **arg)
 {
 	int i;
@@ -7,19 +27,8 @@ void	ft_echo(char **arg)
 	int is_n;
 
 	i = 1;
-	is_n = 0;
 	g_status = 0;
-	while(arg[i])
-	{
-		j = 1;
-		if (arg[i][0] == '-')
-			while (arg[i][j] && arg[i][j] == 'n')
-				j++;
-		if (arg[i][j] != '\0' || j == 1)
-			break;
-		is_n = 1;
-		i++;
-	}
+	is_n = skip_flag(arg, &i);
 	while(arg[i])
 	{
 		ft_putstr_fd(arg[i], STDOUT_FILENO);
