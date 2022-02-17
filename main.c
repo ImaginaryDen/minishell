b6cdc037a38c82edc28d9a9caf5f45b12a5b021c
ImaginaryDen <denis.marakov@mail.ru>
2022-02-17 21:01:22 +0300

    add init_shell

--- main.c
+++ main.c
@@ -22,19 +22,37 @@ char **copy_envp(char **envp)
 	return (copy_envp);
 }
 
+void init_shell(char **envp)
+{
+	char *tmp;
+	int tmp_num;
+
+	g_envp = copy_envp(envp);
+	g_status = 0;
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, sigint_handler);
+	tmp = get_env("SHLVL");
+	if (!tmp)
+		return;
+	tmp = ft_strchr(tmp, '=') + 1;
+	if (!tmp)
+		return;
+	tmp_num = ft_atoi(tmp) + 1;
+	tmp = ft_itoa(tmp_num);
+	set_env("SHLVL", tmp);
+	free(tmp);
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	t_info	info;
 	char	*line;
 
 	info.status = 1;
-	g_envp = copy_envp(envp);
-	g_status = 0;
-	signal(SIGINT, sigint_handler);
-	signal(SIGQUIT, sigint_handler);
+	init_shell(envp);
 	while (info.status)
 	{
-		line = readline("minishell$>");
+		line = readline("minishell$> ");
 		ft_add_history(line);
 		if (line == NULL)
 		{
