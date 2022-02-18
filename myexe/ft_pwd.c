37e80a91680642f1d1188787727c1fcbee291425
ImaginaryDen <denis.marakov@mail.ru>
2022-02-18 21:02:32 +0300

    fixed ft_pwd

--- myexe/ft_pwd.c
+++ myexe/ft_pwd.c
@@ -2,18 +2,16 @@
 
 void ft_pwd(char **arg)
 {
-	char *path;
+	char	*path;
 	
-	path = get_env("PWD");
+	path = getcwd(NULL, 0);
 	g_status = 0;
 	if (!path)
 	{
 		g_status = 1;
 		return;
 	}
-	path = ft_strchr(path, '=');
-	if (!path)
-		return;
-	ft_putstr_fd(path + 1, 1);
+	ft_putstr_fd(path, 1);
 	ft_putstr_fd("\n", 1);
+	free(path);
 }
