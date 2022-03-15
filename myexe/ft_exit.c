9fd4ec26b32414a0cf646cec61dc8668befb81f3
Tomard Jamis <tjamis@su-b2.msk.21-school.ru>
2022-03-15 14:40:47 +0300

    fix exit

--- myexe/ft_exit.c
+++ myexe/ft_exit.c
@@ -3,19 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tanya <tanya@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tjamis <tjamis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 16:11:26 by tjamis            #+#    #+#             */
-/*   Updated: 2022/03/13 18:31:27 by tanya            ###   ########.fr       */
+/*   Updated: 2022/03/15 14:40:22 by tjamis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	exit_with_arg(char **argv)
+{
+	int	size;
+	int	ret;
+
+	size = 0;
+	while (argv[size])
+		size++;
+	if (size > 2 && ft_isdigit(argv[1][0]))
+	{
+		g_info.status = 1;
+		ft_putstr_fd("minishel: exit: too many arguments\n", 2);
+		return (1);
+	}
+	if (argv[1])
+	{
+		ret = ft_atoi(argv[1]);
+		if (!ft_isdigit(argv[1][0]))
+		{
+			ft_putstr_fd("minishel: exit:  numeric argument required\n", 2);
+			g_info.status = 255;
+		}
+		else
+			g_info.status = ret;
+	}
+	return (0);
+}
+
 void	ft_exit(char **argv)
 {
 	if (argv == NULL)
 		ft_putstr_fd("exit\n", 1);
+	else if (exit_with_arg(argv))
+		return ;
 	rl_clear_history();
 	ft_free_array(g_info.history);
 	ft_free_array(g_info.envp);
